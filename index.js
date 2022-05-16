ba84991f4824eaae40194dc2076586d1d06422a2
MdMuzahid07 <MdMuzahid7396@gmail.com>
2022-05-17 02:12:37 +0600

    trying to fixed bug of dashboard component error

--- index.js
+++ index.js
@@ -77,7 +77,9 @@ async function run() {
         app.get('/admin/:email', async(req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({email: email})
-            const isAdmin = user.role == "admin";
+            const isAdmin = user.role === "admin";
+
+            console.log(isAdmin)
             res.send({admin: isAdmin})
         })
 
